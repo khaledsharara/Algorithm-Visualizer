50a96ccb3c9fd9bdec293f359990b6176c8cbea6
khaledsharara <khaled.sharara1@gmail.com>
2024-05-05 02:33:14 +0300

    form width modified

--- src/Components/OSForm.jsx
+++ src/Components/OSForm.jsx
@@ -10,7 +10,6 @@ const OSForm = () => {
   const [position, setPosition] = useState();
   const [highlightedIndex, setHighlightedIndex] = useState(null);
   const [highlightedIndexes, setHighlightedIndexes] = useState([]);
-  const [sortedArray, setSortedArray] = useState([]);
 
   useEffect(() => {
     if (algorithm === "LS" && buttonPressed) {
@@ -91,6 +90,7 @@ const OSForm = () => {
       }
       n--;
     } while (n > 1);
+    setHighlightedIndexes(null);
   };
   const SelectionSort = async () => {
     let arr = [...array];
@@ -113,24 +113,27 @@ const OSForm = () => {
         setArray([...arr]);
       }
     }
+    setHighlightedIndexes(null);
   };
 
   return (
     <>
-      <div className="bg-slate-800 pt-5 min-h-screen max-h-full">
-        <div className="w-1/2 flex mx-auto ">
+<div className="bg-slate-800 pt-5 min-h-screen max-h-full flex justify-content-center">
+  <div className="w-full max-w-3/4" style={{ display: "flex", justifyContent: "center" }}>
           <form
             // onSubmit={handleSubmit}
             className="new-item-form"
           >
             <div className="form-row">
-              <div class="mb-6">
-                <div class="grid grid-cols-4 gap-4">
-                  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white my-auto">
+              <div className="mb-6">
+                <div
+                className={`grid gap-4 ${(algorithm === "LS" || algorithm === "BS")? "grid-cols-4": "grid-cols-3"}`}
+                >
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white my-auto">
                     Choose Algorithm:
                   </label>
                   <select
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     value={algorithm}
                     onChange={(e) => {
                       setAlgorithm(e.target.value);
@@ -145,7 +148,7 @@ const OSForm = () => {
                   <input
                     type="text"
                     id="processes"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Array"
                     value={arrayString}
                     onChange={(e) => setArrayString(e.target.value)}
@@ -155,7 +158,7 @@ const OSForm = () => {
                     <input
                       type="text"
                       id="processes"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Search for"
                       value={search}
                       onChange={(e) => setSearch(Number(e.target.value))}
@@ -169,7 +172,7 @@ const OSForm = () => {
             <div className="flex justify-end">
               <button
                 type="button"
-                class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                 // onClick={handleReset}
               >
                 Reset
@@ -182,18 +185,6 @@ const OSForm = () => {
                 Add
               </button>
             </div>
-
-            {/* <div className="text-right mt-5">
-              <button
-                type="button"
-                className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-                // onClick={() => {
-                //   handleAlgorithm();
-                // }}
-              >
-                Generate
-              </button>
-            </div> */}
 
             <div className="flex flex-row mt-20 h-10">
               {buttonPressed &&
@@ -222,7 +213,7 @@ const OSForm = () => {
             {buttonPressed &&
               position != null &&
               (algorithm === "LS" || algorithm === "BS") && (
-                <label class="block mb-2 mt-10 text-m font-medium text-gray-900 dark:text-white my-auto">
+                <label className="block mb-2 mt-10 text-m font-medium text-gray-900 dark:text-white my-auto">
                   Position: {position}
                 </label>
               )}
